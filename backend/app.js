abb02b407e3974403605735e3500735e29b2c021
Andrej Solovej <andrej.solovej@betgames.tv>
2019-10-14 00:52:06 +0300

    added packages

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+const Recipe = require('./models/recipe');
+
+mongoose.connect('mongodb://root:example@127.0.0.1/')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use('/api/recipe', (req, res, next) => {
+    Recipe.find().then(
+        (recipes) => {
+            res.status(200).json(recipes);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+module.exports = app; 
